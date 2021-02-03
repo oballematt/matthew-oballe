0db6f615332bf36615770ea15afcf4073e354d21
Matthew Oballe <oballematt@gmail.com>
2021-02-03 11:42:40 -0600

    adding fontawesome icons as well as a skills bar to about me page.

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -4,11 +4,9 @@ import styles from './Icon.module.css'
 
 const Icon = (props) => {
     return (
-        <div className={styles.hover}>
-            <a href={props.link} target="_blank" rel='noreferrer'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className={props.iconClass} viewBox="0 0 16 16">
-                <path d={props.iconPath}/>
-                </svg>
+        <div className={styles.backGround}>
+            <a href={props.link}  target="_blank" rel='noreferrer'>
+                <i className={props.icon}></i>
             </a>
         </div>
       
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styles from './Skills.module.css'
+import Icon from '../Icon/Icon'
+
+const skills = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Skills</h1>
+            <Icon />
+            
+        </div>
+    )
+}
+
+export default skills
