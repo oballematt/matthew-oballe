c750e9104e030439c50f0eeea5d599f96e9e5ed5
Matthew Oballe <oballematt@gmail.com>
2021-02-12 14:10:17 -0600

    updated about me section. Fixing screen size issues.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,13 +16,13 @@ const home = () => {
                     <div className={`col-sm-4 ${styles.aboutColumn}`}>
                         <DisplayPicture display='false' picture={profile} />
                         <h1>Matthew Oballe</h1>
-                        <br/>
+                        <br />
                         <h2>Full Stack Developer</h2>
                         <h4>University of Texas Coding Boot Camp</h4>
                         <div className={`row ${styles.iconRow}`}>
                             <Icon
                                 icon='fas fa-envelope fa-3x'
-                                link="mailto:oballematt@gmail.com"/>
+                                link="mailto:oballematt@gmail.com" />
                             <Icon
                                 icon='fab fa-github fa-3x'
                                 link="https://github.com/oballematt" />
@@ -36,7 +36,8 @@ const home = () => {
                     </div>
                     <div className={`col-sm-8 ${styles.aboutColumn}`}>
                         <h1>About Me</h1>
-                        <p>My name is Matt and Im a full stack developer</p>
+                        <p>I started my professional career in the beverage industry, working my way up to production manager at a well known brewery. I loved managing, strategizing and being apart of such a beloved industry. I worked with ferocity and a strong drive to increase my skills in management and become a pivotal part of the company's success. But my passion has always been technology. I decided to make a career change and enroll in the University of Texas Coding Boot Camp. I took my work ethic and dedication from striving to be the best manager I could be, and applied it to learn a number of web development methods in just three months.
+                        During my time in the boot camp, I gained a strong understanding of many elements in programming such as HTML, CSS, ES6, React NodeJs, and databases such as MongoDB and MySQL. I have completed three big projects as well as many smaller projects and I'm ready to create many more with the skillsets I have learned. I look forward to taking on any new challenges web development will throw at me to always increase my knowledge and become a great developer. </p>
                     </div>
                 </div>
             </div>
